279690f2fe9e5fd0f3593c7b61288f87d7f4db7c
vj-arch <57558048+vj-arch@users.noreply.github.com>
2019-12-22 23:19:13 +0530

    Add files via upload

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -12,12 +12,34 @@ import { AuthServiceService } from "../../shared/services/auth-service.service";
 })
 export class SignUpComponent implements OnInit {
   universityNameOb: Observable<any>;
-  universityNames = [];
+  universityNames=[];
+
+//-----------------------------------------------------------------------------------
+bigList: string[] = new Array(10000).fill(0).map((_, i) => i.toString(36) + i);
+  optionList: string[] = [];
+
+  displayTips = true;
+
+  onSearch(value: string): void {
+    if (value && value.length > 1) {
+      this.optionList = this.universityNames[0];
+      this.displayTips = false;
+    } else {
+      this.optionList = [];
+      this.displayTips = true;
+    }
+  }
+
+
+
+
+//------------------------------------------------------------------------------------
+
   constructor(private authService: AuthServiceService) {}
 
   ngOnInit() {
     this.authService.caseStudyFilter().subscribe(universityNames => {
-      this.universityNames.push(universityNames);
+      this.universityNames.push(universityNames[0].properties.country.items.enum);
       console.log(this.universityNames);
     });
   }
